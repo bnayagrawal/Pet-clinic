502aed6ebea230271f79c80844b934a1774800c2
bnayagrawal <bnayagrawal@gmail.com>
2021-01-19 13:42:34 +0530

    Added Pets and Contact info to owenrs on startup.

--- pet-clinic-data/src/main/java/xyz/bnayagrawal/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/xyz/bnayagrawal/petclinic/services/map/OwnerServiceMap.java
@@ -2,13 +2,24 @@ package xyz.bnayagrawal.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import xyz.bnayagrawal.petclinic.model.Owner;
+import xyz.bnayagrawal.petclinic.model.Pet;
 import xyz.bnayagrawal.petclinic.services.CrudService;
 import xyz.bnayagrawal.petclinic.services.OwnerService;
+import xyz.bnayagrawal.petclinic.services.PetService;
+import xyz.bnayagrawal.petclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Owner findById(Long id) {
@@ -22,7 +33,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner> implements OwnerS
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if(object != null) {
+            if(object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null) {
+                        if(pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/xyz/bnayagrawal/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/xyz/bnayagrawal/petclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package xyz.bnayagrawal.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import xyz.bnayagrawal.petclinic.model.Owner;
+import xyz.bnayagrawal.petclinic.model.Pet;
 import xyz.bnayagrawal.petclinic.model.PetType;
 import xyz.bnayagrawal.petclinic.model.Vet;
 import xyz.bnayagrawal.petclinic.services.OwnerService;
 import xyz.bnayagrawal.petclinic.services.PetTypeService;
 import xyz.bnayagrawal.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,31 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("52345254523");
         ownerService.save(owner1);
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("52345254523");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
