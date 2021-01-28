c7a508708654ac4c13a42f02a1fa2b6d5aa3ae35
bnayagrawal <bnayagrawal@gmail.com>
2021-01-28 05:58:38 +0530

    Add Visit map service.

---
+++ pet-clinic-data/src/main/java/xyz/bnayagrawal/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package xyz.bnayagrawal.petclinic.services;
+
+import xyz.bnayagrawal.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit> {
+}
---
+++ pet-clinic-data/src/main/java/xyz/bnayagrawal/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package xyz.bnayagrawal.petclinic.services.map;
+
+import xyz.bnayagrawal.petclinic.model.Owner;
+import xyz.bnayagrawal.petclinic.model.Pet;
+import xyz.bnayagrawal.petclinic.model.Visit;
+import xyz.bnayagrawal.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit> implements VisitService {
+    
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        final Pet pet = object.getPet();
+        final Owner owner = null == pet ? null : pet.getOwner();
+
+        if(pet == null || pet.getId() == null || owner == null || owner.getId() == null)
+            throw new RuntimeException("Invalid Visit");
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
